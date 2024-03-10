8d7610e422bcd7350fb89de01a18a1c1f85e1d7b
icenspln <icenboat@gmail.com>
2024-03-11 00:23:50 +0100

    model update

--- app/components/model.tsx
+++ app/components/model.tsx
@@ -1,5 +1,11 @@
-import { Environment, OrbitControls, Text } from "@react-three/drei"
+import {
+    Environment,
+    MeshTransmissionMaterial,
+    OrbitControls,
+    Text,
+} from "@react-three/drei"
 import { Canvas, useFrame } from "@react-three/fiber"
+import { useControls } from "leva"
 import { useRef } from "react"
 
 const Model = () => {
@@ -7,7 +13,7 @@ const Model = () => {
         <Canvas id="canvas" className="basis-1/3">
             <OrbitControls enableZoom={false} enablePan={false} />
             <directionalLight intensity={3} position={[0, 3, 2]} />
-            <Environment preset="city" />
+            <Environment preset="warehouse" />
             <Object />
         </Canvas>
     )
@@ -22,12 +28,25 @@ const Object = () => {
         mesh.current.rotation.z += delta * 0.2
     })
 
+    const materialProps = useControls({
+        thickness: { value: 0.8, min: 0, max: 3, step: 0.05 },
+        roughness: { value: 0.2, min: 0, max: 1, step: 0.1 },
+        transmission: { value: 1, min: 0, max: 1, step: 0.1 },
+        ior: { value: 1.7, min: 0, max: 3, step: 0.1 },
+        chromaticAberration: { value: 1, min: 0, max: 1 },
+        backside: { value: true },
+    })
+
     return (
-        <mesh ref={mesh}>
-            <tetrahedronGeometry args={[3, 0]} />
-            <meshStandardMaterial color={"silver"} />
-            {/* <Text>Hello World</Text> */}
-        </mesh>
+        <group>
+            {/* <Text fontSize={2} font="" position={[0, 0, -4.5]}>
+                Hello World
+            </Text> */}
+            <mesh ref={mesh}>
+                <tetrahedronGeometry args={[3, 0]} />
+                <MeshTransmissionMaterial color={"silver"} {...materialProps} />
+            </mesh>
+        </group>
     )
 }
 
